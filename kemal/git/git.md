4210faa5aec649845e9f6c703cccc4fe9a855aa0
devopsyolu <devopsyolu@gmail.com>
2024-12-22 23:03:15 +0100

    kemal folder eklendi

---
+++ kemal/git/git.md
@@ -0,0 +1,6 @@
+# Git Kullanim Rehberi
+
+Bu dokuman, Git hizmetlerinin temel kullanimini anlamaniza ve projelerinizi Git platformunda basarili bir sekilde yurutmenize yardimci olmayi amaclamaktadir.
+
+## Baslangic
+
---
+++ kemal/linux/shell/server-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Log dosyası yolu
+LOG_FILE="/tmp/update-loglari.txt"
+
+# Güncelleme işlemlerini log dosyasına yazdır
+{
+    echo "Güncelleme Başladı: $(date)"
+    sudo apt update && sudo apt upgrade -y
+    echo "Güncelleme Tamamlandı: $(date)"
+} >> "$LOG_FILE" 2>&1
